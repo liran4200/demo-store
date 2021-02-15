2c30d76551dd9caba9ec4676f548ef2c5c460de5
liran4200 <liran.yehudar@sap.com>
2021-02-16 00:55:51 +0200

    add category

---
+++ src/main/java/com/example/service/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+public class ProductNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
